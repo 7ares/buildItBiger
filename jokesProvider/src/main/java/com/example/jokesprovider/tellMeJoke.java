b1b8adbb7e1ab0be1ce36aaab0b84c3522d475bb
7ares <7aresmohamed@gmail.com>
2019-03-20 17:41:28 +0200

    generate java library and provide a joke

---
+++ jokesProvider/src/main/java/com/example/jokesprovider/tellMeJoke.java
@@ -0,0 +1,11 @@
+package com.example.jokesprovider;
+
+public class tellMeJoke {
+    public String getJoke(){
+        return "Knock! Knock!\n" +
+                "Who’s there?\n" +
+                "Candice.\n" +
+                "Candice who?\n" +
+                "Candice door open, or am I stuck out here?";
+    }
+}
