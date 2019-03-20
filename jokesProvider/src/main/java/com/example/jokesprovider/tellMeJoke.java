acb8c38d2614d1972e28a2fc9856bd67e21fff22
7ares <7aresmohamed@gmail.com>
2019-03-20 17:42:56 +0200

    show joke in android module layout

--- jokesProvider/src/main/java/com/example/jokesprovider/tellMeJoke.java
+++ jokesProvider/src/main/java/com/example/jokesprovider/tellMeJoke.java
@@ -3,7 +3,7 @@ package com.example.jokesprovider;
 public class tellMeJoke {
     public String getJoke(){
         return "Knock! Knock!\n" +
-                "Who’s there?\n" +
+                "Who\'s there?\n" +
                 "Candice.\n" +
                 "Candice who?\n" +
                 "Candice door open, or am I stuck out here?";
--- jokesdisplayer/src/main/java/com/example/jokesdisplayer/DisplayJokeActivity.java
+++ jokesdisplayer/src/main/java/com/example/jokesdisplayer/DisplayJokeActivity.java
@@ -2,6 +2,7 @@ package com.example.jokesdisplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DisplayJokeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_joke);
+        String joke = getIntent().getStringExtra("joke");
+        TextView showJokeTextView = findViewById(R.id.showJoke_tv);
+        showJokeTextView.setText(joke);
+
     }
 }
